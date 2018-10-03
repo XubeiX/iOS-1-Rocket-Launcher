55ce87669259ce0e0dab9038b71dc6b6bb90cb7a
Artur Ratajczak <artur.ratajczak1@gmail.com>
2018-10-03 10:57:25 -0700

    iOS 1st easy app

--- Rocket Launcher/ViewController.swift
+++ Rocket Launcher/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var RocketLbl: UILabel!
+
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let musicPath = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: musicPath)
+        
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        
     }
 
 
+    
+    @IBAction func PowerBtnPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerBtn.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 20, width: 375, height: 676)
+            self.RocketLbl.isHidden = false
+        })
+    }
+    
 }
 
